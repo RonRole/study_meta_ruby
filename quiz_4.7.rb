8027ee81c3b8d7377726ffe636cbe97d5ace0a10
ラッコ <50513757+RonRole@users.noreply.github.com>
2021-05-12 01:19:47 -0700

    Create quiz_4.7.rb

---
+++ quiz_4.7.rb
@@ -0,0 +1,17 @@
+def setup(&block)
+    @env.instance_eval {
+        @setups ||= []
+        @setups << block
+    }
+end
+
+def event(description, &block)
+    @env.instance_eval {
+        @setups.each(&:call)
+        puts "ALERT:#{description}" if block.call
+    }
+end
+
+@env = Object.new
+
+load('events')
